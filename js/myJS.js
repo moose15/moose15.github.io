6e111f2c10f9d101b655f3f657c4e3035441dc3f
Katie Pustolski <pustolskik@gmail.com>
2019-03-11 18:45:48 -0700

    Updated design of my portfolio and also updated my resume.

--- js/myJS.js
+++ js/myJS.js
@@ -1,77 +1,42 @@
-//Katie Pustolski
-// last updated: Jan. 2017
+// By Chris Coyier & tweaked by Mathias Bynens
 
-  "use strict";
-// javaScript (jquery) for logo
-//logo fades out as user scrolls down page and 
-// fades in when user scrolls up page
-  $(window).scroll(function() {
+$(function() {
 
-    // var SCROLL_NUM = 50;
-    //   var height = $(window).scrollTop();
+	// Find all YouTube videos
+	var $allVideos = $("iframe[src^='http://www.youtube.com']"),
 
-    //   if(height  > SCROLL_NUM) {
-    //     // logo.fadeOut("slow");
-    //    document.getElementById("navScroll").style.backgroundColor="rgba(8, 83, 107, .95)";
-    //     // console.log(100);
-    //       // do something
-    //   }
-    //   if(height < SCROLL_NUM){
-    //     // logo.fadeIn("slow");
-    //   document.getElementById("navScroll").style.backgroundColor="rgba(8, 83, 107, 0)";
+	    // The element that is fluid width
+	    $fluidEl = $(".videoWrapper");
 
-    //   }
-  });
-// show the loader while the page is loading. Once loaded, fade out
- $(window).load(function () {
+	// Figure out and save aspect ratio for each video
+	$allVideos.each(function() {
 
-      $("#loadScreen").fadeOut("slow");
+		$(this)
+			.data('aspectRatio', this.height / this.width)
+			
+			// and remove the hard coded width/height
+			.removeAttr('height')
+			.removeAttr('width');
 
-  });
-  
-  // adding alert to blog link
-$(".blogLink").click(function(){
+	});
 
-  var m = confirm("This will open a new tab to katiepustolski.tumblr.com. \n\n Select 'ok' to continue.");
+	// When the window is resized
+	// (You'll probably want to debounce this)
+	$(window).resize(function() {
 
-  if(m){
-     window.open("http://katiepustolski.tumblr.com/");
-    // window.open("katiepustolski.tumblr.com");
-  }
-  else{
+		var newWidth = $fluidEl.width();
+		
+		// Resize all videos according to their own aspect ratio
+		$allVideos.each(function() {
 
-  }
+			var $el = $(this);
+			$el
+				.width(newWidth)
+				.height(newWidth * $el.data('aspectRatio'));
+
+		});
+
+	// Kick off one resize to fix all videos on page load
+	}).resize();
+
 });
-
- // Clicking a link in the nva bar will scroll down the page using this code...
-  // Add smooth scrolling to all links
-  // http://stackoverflow.com/questions/4198041/jquery-smooth-scroll-to-an-anchor
-  var hashTagActive = "";
-    $(".scrollLink").click(function (event) {
-        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
-            event.preventDefault();
-            //calculate destination place
-            var dest = 0;
-            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-                dest = $(document).height() - $(window).height();
-            } else {
-                dest = $(this.hash).offset().top;
-            }
-            //go to destination
-            $('html,body').animate({
-                scrollTop: dest
-            }, 500, 'swing');
-            hashTagActive = this.hash;
-        }
-    });
-
-//hidden text to show up
-
-$(".st0").mouseenter(function(){
-
-  $(".hidden").fadeIn(50);
-
-}).mouseleave(function(){
-  $(".hidden").fadeOut(50);
-});
-
